19d6b6fef361f65ea6f797045a18c5d5e68ab74f
robbiecrabtree <robbie.crabtree@kainos.com>
2023-08-24 13:50:43 +0100

    GET Delivery Employee

--- src/main/java/org/kainos/ea/resources/DeliveryEmployeeController.java
+++ src/main/java/org/kainos/ea/resources/DeliveryEmployeeController.java
@@ -1,15 +1,15 @@
 package org.kainos.ea.resources;
 
-import jdk.jfr.internal.Repository;
 import org.kainos.ea.api.DeliveryEmployeeService;
-import org.kainos.ea.cli.DeliveryEmployee;
 import org.kainos.ea.cli.DeliveryEmployeeRequest;
 import org.kainos.ea.cli.DeliveryEmployeeUpdateRequest;
-import org.kainos.ea.client.*;
+import org.kainos.ea.client.FailedToCreateDeliveryEmployee;
+import org.kainos.ea.client.FailedToGetDeliveryEmployee;
+import org.kainos.ea.client.FailedToUpdateDeliveryEmployee;
+import org.kainos.ea.client.DeliveryEmployeeDoesNotExistException;
 import org.kainos.ea.core.DeliveryEmployeeValidator;
 import org.kainos.ea.db.DeliveryDao;
 
-import javax.print.attribute.standard.Media;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -28,12 +28,8 @@ public class DeliveryEmployeeController {
     @Produces(MediaType.APPLICATION_JSON)
     public Response createDeliveryEmployee(DeliveryEmployeeRequest employeeToCreate){
         try{
-            deliveryService.createDeliveryEmployee(employeeToCreate);
-            return Response.status(Response.Status.CREATED).build();
+            return Response.ok(deliveryService.createDeliveryEmployee(employeeToCreate)).build();
         } catch (FailedToCreateDeliveryEmployee e) {
-            System.err.println(e.getMessage());
-            return Response.serverError().build();
-        } catch (FailedToValidateEmployee e) {
             System.err.println(e.getMessage());
             return Response.serverError().build();
         }
@@ -44,41 +40,32 @@ public class DeliveryEmployeeController {
     public Response updateDeliveryEmployee(@PathParam("id") int id, DeliveryEmployeeUpdateRequest employeeToUpdate)
     {
         try{
-            String validation = deliveryEmployeeValidator.isDeliveryUpdateValid(employeeToUpdate);
-            if(validation != null)
-            {
-                System.err.println(validation);
-                return Response.status(Response.Status.BAD_REQUEST).build();
-            }
             deliveryService.updateDeliveryEmployee(id,employeeToUpdate);
             return Response.ok().build();
         } catch (FailedToUpdateDeliveryEmployee e) {
             System.err.println(e.getMessage());
             return  Response.serverError().build();
-        } catch (DeliveryEmployeeDoesNotExist e) {
+        } catch (DeliveryEmployeeDoesNotExistException e) {
             System.err.println(e.getMessage());
             return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
-    @DELETE
-    @Path("/delivery/{id}")
+
+    @GET
+    @Path("/deliveryEmployee/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response deleteDeliveryEmployee(@PathParam("id") int id)
+    public Response getDeliveryEmployee(@PathParam("id") int id)
     {
-        try
-        {
-            if(!deliveryEmployeeValidator.doesEmployeeExist(id))
-            {
-                System.err.println("Employee does not exist.");
-                return Response.status(Response.Status.BAD_REQUEST).build();
-            }
-            deliveryService.deleteOrder(id);
-            return Response.ok().build();
-        } catch (SQLException | FailedToDeleteDeliveryEmployee e) {
+        try {
+            return Response.ok(deliveryService.getDeliveryEmployee(id)).build();
+        } catch (FailedToGetDeliveryEmployee e) {
             System.err.println(e.getMessage());
             return Response.serverError().build();
+        } catch (DeliveryEmployeeDoesNotExistException e) {
+            System.err.println(e.getMessage());
+            return Response.status(Response.Status.BAD_REQUEST).build();
         }
     }
 }
